2e9225db1ef7e98cfd32a01210f0b82041f74883
saurav529 <saurav.bhandari@yahoo.com>
2019-08-25 10:24:07 +0545

    Location Geocodes

---
+++ app/src/main/java/com/deercoders/smartwastemanagementsystem/SaveLocationData.java
@@ -0,0 +1,14 @@
+package com.deercoders.smartwastemanagementsystem;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+class SaveLocationData {
+
+
+}
+
--- app/src/main/java/com/deercoders/smartwastemanagementsystem/UserDashboard.java
+++ app/src/main/java/com/deercoders/smartwastemanagementsystem/UserDashboard.java
@@ -18,7 +18,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Locale;
 
@@ -34,94 +45,126 @@ public class UserDashboard extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button button = findViewById(R.id.load_location);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
+        final TextView locationDisplay = findViewById(R.id.location_value);
+        final TextView speedDisplay = findViewById(R.id.speed);
+        final TextView longitudeDisplay = findViewById(R.id.longitude);
+        final TextView latitudeDisplay = findViewById(R.id.latitude);
+        TrackerSettings settings =
+                new TrackerSettings()
+                        .setUseGPS(true)
+                        .setUseNetwork(true)
+                        .setUsePassive(true)
+                        .setTimeBetweenUpdates(60 * 60)
+                        .setMetersBetweenUpdates(2);
 
-                final TextView locationDisplay = findViewById(R.id.location_value);
-                final TextView speedDisplay = findViewById(R.id.speed);
-                final TextView longitudeDisplay = findViewById(R.id.longitude);
-                final TextView latitudeDisplay = findViewById(R.id.latitude);
-                TrackerSettings settings =
-                        new TrackerSettings()
-                                .setUseGPS(true)
-                                .setUseNetwork(true)
-                                .setUsePassive(true)
-                                .setTimeBetweenUpdates(10)
-                                .setMetersBetweenUpdates(2);
+        LocationTracker tracker = new LocationTracker(UserDashboard.this, settings) {
 
-                LocationTracker tracker = new LocationTracker(UserDashboard.this, settings) {
-
-                    @Override
-                    public void onLocationFound(Location location) {
-                        float bearing = location.getBearing();
-                        String data = "" + bearing;
+            @Override
+            public void onLocationFound(Location location) {
+                float bearing = location.getBearing();
+                String data = "" + bearing;
 //                        locationDisplay.setText(data);
 //
 //                        float speed = location.getSpeed();
 //                        data = "" + speed;
 //                        speedDisplay.setText(data);
 
-                        double longitude = location.getLongitude();
-                        data = "" + longitude;
-                        longitudeDisplay.setText(data);
+                double longitude = location.getLongitude();
+                data = "" + longitude;
+                longitudeDisplay.setText(data);
 
-                        double latitude = location.getLatitude();
-                        data = "" + latitude;
-                        latitudeDisplay.setText(data);
+                double latitude = location.getLatitude();
+                data = "" + latitude;
+                latitudeDisplay.setText(data);
 
-                        Geocoder geocoder;
-                        List<Address> addresses;
-                        geocoder = new Geocoder(UserDashboard.this, Locale.getDefault());
+                Geocoder geocoder;
+                List<Address> addresses;
+                geocoder = new Geocoder(UserDashboard.this, Locale.getDefault());
 
-                        try {
-                            double lat = location.getLatitude();
-                            double lon = location.getLongitude();
-                            addresses = geocoder.getFromLocation(lat, lon, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-                            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-                            String city = addresses.get(0).getLocality();
-                            String state = addresses.get(0).getAdminArea();
-                            String country = addresses.get(0).getCountryName();
-                            String postalCode = addresses.get(0).getPostalCode();
-                            String knownName = addresses.get(0).getFeatureName();
-                            System.out.println("\n============================================================================================================================================");
-                            System.out.println("address = " + address);
-                            System.out.println("city = " + city);
-                            System.out.println("state = " + state);
-                            System.out.println("country = " + country);
-                            System.out.println("postalCode = " + postalCode);
-                            System.out.println("knownName = " + knownName);
-                            System.out.println("============================================================================================================================================\n");
-                            locationDisplay.setText(address);
-                            speedDisplay.setText(city);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                try {
+                    double lat = location.getLatitude();
+                    double lon = location.getLongitude();
+                    addresses = geocoder.getFromLocation(lat, lon, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                    String city = addresses.get(0).getLocality();
+                    String state = addresses.get(0).getAdminArea();
+                    String country = addresses.get(0).getCountryName();
+                    String postalCode = addresses.get(0).getPostalCode();
+                    String knownName = addresses.get(0).getFeatureName();
+                    System.out.println("\n============================================================================================================================================");
+                    System.out.println("address = " + address);
+                    System.out.println("city = " + city);
+                    System.out.println("state = " + state);
+                    System.out.println("country = " + country);
+                    System.out.println("postalCode = " + postalCode);
+                    System.out.println("knownName = " + knownName);
+                    System.out.println("============================================================================================================================================\n");
+                    locationDisplay.setText(address);
+                    speedDisplay.setText(city);
 
+                    if (address != null) {
+                        String addr = address.split(",")[0].replace(" ", "%20");
+//                                URL url = new URL("http://192.168.137.188:8080/location/save?user=1&address=" + addr + "&latitude=" + latitude + "&longitude=" + longitude);
+//                                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//                                urlConnection.disconnect();
 
+                        // Instantiate the RequestQueue.
+                        RequestQueue queue = Volley.newRequestQueue(UserDashboard.this);
+                        String url = "http://192.168.137.58:8080/location/save?user=1&address=" + encodeValue(addr) + "&latitude=" + latitude + "&longitude=" + longitude;
+                        System.out.println(url);
+
+                        JsonObjectRequest request = new JsonObjectRequest(url, null,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        if (null != response) {
+                                            Toast.makeText(UserDashboard.this, "ADDRESS SENT", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                        queue.add(request);
                     }
 
-                    @Override
-                    public void onTimeout() {
 
-                    }
-                };
-                if (ActivityCompat.checkSelfPermission(UserDashboard.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(UserDashboard.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                tracker.startListening();
+            }
 
+            @Override
+            public void onTimeout() {
 
             }
-        });
+        };
+        if (ActivityCompat.checkSelfPermission(UserDashboard.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(UserDashboard.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        tracker.startListening();
 
+
+    }
+
+
+    public static String encodeValue(String value) {
+        try {
+            return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex.getCause());
+        }
     }
 
     @Override
